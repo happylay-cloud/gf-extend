e5a3a5c2a3338453a04aedcd93a629576fdd66d1
happylay <1803455421@qq.com>
2021-01-16 09:17:18 +0800

    🌈 修正包依赖

--- compoent/gfmiddleware/gf_handler.go
+++ compoent/gfmiddleware/gf_handler.go
@@ -3,7 +3,7 @@ package gfmiddleware
 import (
 	"github.com/gogf/gf/frame/g"
 	"github.com/gogf/gf/net/ghttp"
-	"github.com/happylay-cloud/gf-extend/common/gfresponse"
+	"github.com/happylay-cloud/gf-extend/common/gfres"
 )
 
 // MiddlewareErrorHandler 全局后置中间件捕获异常
