9ed6d076df702aa542ef3c46604d8685ba116aa2
happylay <1803455421@qq.com>
2021-01-29 21:20:46 +0800

    🔥 更新SonyFlake

--- common/hutils/hid/snoyflake_test.go
+++ common/hutils/hid/snoyflake_test.go
@@ -9,27 +9,32 @@ import (
 )
 
 func TestSonyFlake(t *testing.T) {
-	parse, _ := time.Parse(time.RFC3339, "2000-01-01")
+	parse, _ := time.Parse(time.RFC3339, "2021-01-01")
 
 	settings := Settings{
 		StartTime: parse,
 	}
 
 	sf := NewSonyFlake(settings)
+
+	// 3395,6479,3034,2449,89
+	// 339564902606177149
+	// 339564939314791293
+	// 339564980184089469
+	// 339565231053800317
+	// 339565309868049277
+	// 339565349785502589
 	go func() {
-		for i := 0; i < 10; i++ {
+		for i := 0; i < 100000; i++ {
 			id, err := sf.NextID()
 			if err != nil {
 				fmt.Println(err)
 			}
 			fmt.Println(id)
+			decompose := Decompose(id)
+			g.Dump(decompose)
 		}
 	}()
-
-	decompose := Decompose(339556772367303549)
-
-	fmt.Println(1 / 0.000001)
-	g.Dump(decompose)
 
 	select {}
 }
