c6e734f69135cb506f365f4fd9224ab06f15070e
happylay <1803455421@qq.com>
2022-01-26 21:39:06 +0800

    💖【功能】打印缓存真实安装路径

--- common/hcache/hcache_bean.go
+++ common/hcache/hcache_bean.go
@@ -1,8 +1,12 @@
 package hcache
 
 import (
-	"errors"
+	"github.com/gogf/gf/frame/g"
+	"github.com/gogf/gf/text/gstr"
 	"github.com/xujiajun/nutsdb"
+
+	"errors"
+	"os"
 	"sync"
 )
 
@@ -32,6 +36,21 @@ func GetNutsDbCacheBean() (*nutsdb.DB, error) {
 			nutsDbCacheErr = err
 		}
 		nutsDbCache = db
+
+		// 解析安装真正路径
+		var realPath string
+
+		if gstr.HasPrefix(nutsDbPath, "./") {
+			pwd, err := os.Getwd()
+			if err != nil {
+				nutsDbCacheErr = err
+			}
+			realPath = pwd + gstr.TrimLeftStr(nutsDbPath, ".")
+		} else {
+			realPath = nutsDbPath
+		}
+
+		g.Log().Line(false).Info("完成NutsDb缓存数据库实例化，当前安装路径：" + realPath)
 	})
 
 	return nutsDbCache, nutsDbCacheErr
@@ -40,6 +59,7 @@ func GetNutsDbCacheBean() (*nutsdb.DB, error) {
 // InitSetNutsDbGlobalPath 初始化NutsDb数据库安装路径，仅初始化一次，默认安装位置：/tmp/hcache/nutsdb/.cache/db
 func InitSetNutsDbGlobalPath(dir string) error {
 	if nutsDbCache != nil {
+		g.Log().Line(false).Error("初始化NutsDb数据库安装路径失败，已初始化位置：" + nutsDbPath)
 		return errors.New("初始化NutsDb数据库安装路径失败，已初始化位置：" + nutsDbPath)
 	}
 	// 更改默认安装路径
