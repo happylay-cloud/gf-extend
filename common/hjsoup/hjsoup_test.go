f4864ab255764df25baa1efbb94b5405a4be071c
happylay <1803455421@qq.com>
2022-01-27 12:28:20 +0800

    🌜【功能】解决协程通道关闭问题

--- common/hjsoup/hjsoup_test.go
+++ common/hjsoup/hjsoup_test.go
@@ -1,10 +1,13 @@
 package hjsoup
 
 import (
-	"fmt"
-	"testing"
-
 	"github.com/gogf/gf/frame/g"
+	"github.com/gogf/gf/text/gstr"
+	"github.com/google/uuid"
+
+	"fmt"
+	"sync"
+	"testing"
 )
 
 func TestHttpClient(t *testing.T) {
@@ -26,4 +29,49 @@ func TestSearchByProductCodeCache(t *testing.T) {
 	}
 
 	g.Dump(productCodeInfo)
+}
+
+func TestChannel(t *testing.T) {
+
+	// 创建验证码通道
+	doorCode := make(chan string)
+
+	// 设置计数器
+	count := 300
+
+	// 创建同步等待组
+	wg := &sync.WaitGroup{}
+	wg.Add(count)
+
+	go deferCloseChannel(doorCode, wg)
+
+	// 执行多个协程
+	for x := 1; x <= count; x++ {
+		go doSomething(x, doorCode, wg)
+	}
+
+	for data := range doorCode {
+		fmt.Println("读取业务返回数据：", data)
+	}
+
+}
+
+func doSomething(x int, doorCode chan string, wg *sync.WaitGroup) {
+	// 查询数据
+	data := uuid.New().String()
+	// 处理业务数据
+	if gstr.LenRune(data) > 0 {
+		doorCode <- data
+	}
+	fmt.Println("执行协程业务：", x)
+	// 计数器减一
+	wg.Done()
+}
+
+func deferCloseChannel(doorCode chan string, wg *sync.WaitGroup) {
+	// 子线程等待
+	wg.Wait()
+	// 关闭通道
+	close(doorCode)
+	fmt.Println("关闭channel通道")
 }
