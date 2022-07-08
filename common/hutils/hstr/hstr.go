e18e6044a0cd1c6c686392c7e54043ec085819e4
happylay <1803455421@qq.com>
2022-07-08 23:44:03 +0800

    🌜 【功能】新增转换特殊空格方法

--- common/hutils/hstr/hstr.go
+++ common/hutils/hstr/hstr.go
@@ -4,12 +4,15 @@ import (
 	"bytes"
 	"github.com/gogf/gf/text/gstr"
 	"strconv"
+	"strings"
 )
 
 // TrimBlank 删除字符串首尾空白
 func TrimBlank(str string) string {
+	// 去除空白字符及特殊空白
+	trimStr := strings.TrimSpace(str)
 	// 去除制表符
-	trimStr := gstr.TrimStr(str, "\t")
+	trimStr = gstr.TrimStr(trimStr, "\t")
 	// 去除换行符
 	trimStr = gstr.TrimStr(trimStr, "\n")
 	// 去除首尾空白字符
@@ -21,6 +24,17 @@ func ZhToUnicode(zhContent string) string {
 	textQuoted := strconv.QuoteToASCII(zhContent)
 	// 去除引号
 	return textQuoted[1 : len(textQuoted)-1]
+}
+
+// SpecialEmptyToNormalEmpty 特殊空白转换成普通空白
+func SpecialEmptyToNormalEmpty(content string) string {
+	// 转换成unicode编码
+	unicodeText := ZhToUnicode(content)
+	// 转换全角空格
+	unicodeText = gstr.Replace(unicodeText, "\\u00a0", " ")
+	// 转换半角空格
+	unicodeText = gstr.Replace(unicodeText, "\\u2003", " ")
+	return UnicodeToZh(unicodeText)
 }
 
 // UnicodeToZh unicode转中文
--- common/hutils/hstr/hstr_test.go
+++ common/hutils/hstr/hstr_test.go
@@ -2,9 +2,27 @@ package hstr
 
 import (
 	"fmt"
+	"strings"
 	"testing"
 )
 
 func TestTrimBlank(t *testing.T) {
 	fmt.Println(TrimBlank(" \t这是一个去除首尾空白字符的测试。 \n "))
+}
+
+func TestZhBlank(t *testing.T) {
+	// 开始 占位符 占位符 结束
+	text := " 开始 占位符 占位符 结束 "
+
+	// 转换普通字符
+	normalEmpty := SpecialEmptyToNormalEmpty(text)
+	fmt.Println(normalEmpty)
+	fmt.Println(ZhToUnicode(normalEmpty))
+
+	// 去除首尾空白
+	emptyText := strings.TrimSpace(text)
+	fmt.Println(emptyText)
+	// 查看编码
+	fmt.Println(ZhToUnicode(emptyText))
+
 }
